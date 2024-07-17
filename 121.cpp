2793c12fb4fb144e8a144fe9fd563c78580d9098
devp299 <d.h.vadharodia@gmail.com>
2024-07-17 09:08:38 +0530

    Second day challenge completed

---
+++ 121.cpp
@@ -0,0 +1,19 @@
+// Best Time to Buy and Sell Stock
+
+// LeetCode Solution: 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maxProfit = 0;
+        int mini = prices[0];
+
+        for(int i=1;i<n;i++){
+            mini = min(mini,prices[i]);
+            int profit = prices[i] - mini;
+            maxProfit = max(profit,maxProfit);
+        }
+
+        return maxProfit;
+    }
+};
---
+++ 974.cpp
@@ -0,0 +1,23 @@
+// Subarray Sums Divisible by K
+
+// LeetCode Solution:
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int count = 0;
+        
+        // Iterate through all possible subarrays
+        for (int i = 0; i < nums.size(); i++) {
+            int sum = 0;
+            for (int j = i; j < nums.size(); j++) {
+                sum += nums[j];
+                // Check if the sum is divisible by k
+                if (sum % k == 0) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
